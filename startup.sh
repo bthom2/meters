65e0b57889c960dd904c6d77fa047341951cb941
bthom2 <thomasbrian570@gmail.com>
2022-04-08 02:27:27 -0500

    startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-service ssh start
 echo "export MQTT_Password=$MQTT_Password" >> /root/.bashrc
 echo "export MQTT_User=$MQTT_User" >> /root/.bashrc
 echo "export MQTT_Broker=$MQTT_Broker" >> /root/.bashrc
@@ -9,3 +8,5 @@ echo "export Meter1_ID=$Meter1_ID" >> /root/.bashrc
 echo "export Meter1_CRC=$Meter1_CRC" >> /root/.bashrc
 echo "export PYTHONPATH=$PYTHONPATH" >> /root/.bashrc
 echo "export DISPLAY=$DISPLAY" >> /root/.bashrc
+
+exec "service ssh start"
