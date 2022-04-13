7d055d1f15124b1fcccac92df50f5bd0b7cf8bac
bthom2 <thomasbrian570@gmail.com>
2022-04-12 23:09:13 -0500

    updated startup

--- startup.sh
+++ startup.sh
@@ -11,6 +11,6 @@ echo "export DISPLAY=$DISPLAY" >> /root/.bashrc
 
 (cd /opt/meters && exec git pull origin master)
 
-python2.7 /opt/meters/mqtt.py
+exec python2.7 /opt/meters/mqtt.py
 
 service ssh start
