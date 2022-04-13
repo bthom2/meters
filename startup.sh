8b1ebeee1ecd810cb68cc0124a3ecffe3111d837
bthom2 <thomasbrian570@gmail.com>
2022-04-13 02:36:35 -0500

    startup9

--- startup.sh
+++ startup.sh
@@ -13,8 +13,8 @@ echo "export DISPLAY=$DISPLAY" >> /root/.bashrc
 (cd /opt/meters && exec git pull)
 chmod +x /opt/meters/startup.sh
 
-python3 /opt/meters/fhss_detector_reference_rtlsdr.py &
+nohup python3 /opt/meters/fhss_detector_reference_rtlsdr.py 
 sleep 10
-exec python2.7 /opt/meters/mqtt.py 127.0.0.1 5002
+python2.7 /opt/meters/mqtt.py 127.0.0.1 5002
 
 service ssh start
