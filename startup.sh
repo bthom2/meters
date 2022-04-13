36e148e7aac7925e297c455218d9cbbd1b226847
bthom2 <thomasbrian570@gmail.com>
2022-04-13 02:51:59 -0500

    h

--- startup.sh
+++ startup.sh
@@ -16,6 +16,6 @@ chmod +x /opt/meters/startup.sh
 service ssh start
 
 python3 /opt/meters/fhss_detector_reference_rtlsdr.py & 
-exec sleep 10
-exec python2.7 /opt/meters/mqtt.py 127.0.0.1 5002 &
+sleep 10
+python2.7 /opt/meters/mqtt.py 127.0.0.1 5002 &
 
